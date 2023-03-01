0ff89ee9b6aaa77b3d16876ad6cad315d4c42348
Ambre Giraud-Lanza <agiraudlanza@juniorisep.com>
2023-03-01 23:07:15 +0100

    creation page contact

--- src/Component/Pages/Contact.tsx
+++ src/Component/Pages/Contact.tsx
@@ -1,9 +1,12 @@
 //page de contact
+import "./Contact.css"
+
 function Contact() {
   return (
     <div>
       <title>Nous contacter</title>
       <h1>Nous contacter</h1>
+      <br />
       <div id="icones">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
@@ -25,7 +28,21 @@ function Contact() {
       </div>
 
       <div id='formulaire'>
+        <form>
+          <label>
+    Votre nom     
+            <input type="text" name="name" />
+            <br /><br />
+    Votre email     
+            <input type="text" name="email" />
+            <br /><br />
 
+            <textarea>
+  Votre message
+            </textarea>
+          </label><br />
+          <input type="submit" value="Submit" />
+        </form>
 
       </div>
     </div>
