c4b9620577f872ad76d47f33ff9d3452ef305fed
Ambre Giraud-Lanza <agiraudlanza@juniorisep.com>
2023-03-04 18:00:56 +0100

    création page mention légale

--- src/Component/Pages/mentions.tsx
+++ src/Component/Pages/mentions.tsx
@@ -6,10 +6,20 @@ function Mentions() {
   return (
     <div>
       <Navigation />
+      <title>Mentions légales</title>
+      <br />
+      <p className="text-4xl md:text-lg">Mentions légales</p>     
+      <p className="m-16 text-justify ">
 
-      <button className="btn btn-primary">One</button>
-      <button className="btn btn-secondary">Two</button>
-      <button className="btn btn-accent btn-outline">Three</button>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu feugiat massa. Proin faucibus mauris ut nulla viverra tempor. Etiam elementum porttitor leo, et facilisis nunc. Vivamus eget nibh tincidunt, posuere odio in, iaculis tellus. Nulla et magna a nisl pretium facilisis sit amet eget mi. Integer scelerisque mi ac enim convallis interdum. Ut eleifend lorem lacus, sit amet bibendum tellus ultrices non. Vestibulum mattis, nulla sed aliquam pellentesque, turpis nisi varius tortor, eu posuere dolor nunc id risus. Duis iaculis purus ante. Pellentesque vestibulum, sem et auctor tristique, magna neque egestas mauris, et fringilla ex nulla sit amet nibh. Nullam porttitor odio est, vel tempor urna vehicula in. Praesent et velit varius, porta nunc hendrerit, eleifend diam. Cras ut pretium nibh, vel egestas nisi.
+
+Maecenas sit amet pulvinar libero, eget commodo libero. Donec sed fringilla lectus. In ac venenatis arcu. Integer vel malesuada ligula, pulvinar mollis ipsum. Mauris et sapien et augue eleifend volutpat et id nulla. Sed lacus eros, elementum consequat aliquam ac, eleifend in ligula. Sed dignissim mi et tempus tempor. Suspendisse potenti. Suspendisse ut mattis ante. Donec varius nec nulla eget ultrices. Maecenas vel sagittis mauris. Integer ac neque nisi. Maecenas congue vehicula ligula a ultrices.
+
+Pellentesque pulvinar tincidunt nibh, et imperdiet ligula maximus et. Quisque est sapien, maximus id ullamcorper at, auctor eu arcu. Vivamus congue volutpat odio, id finibus urna fringilla nec. Integer eu quam tristique, venenatis diam sit amet, sollicitudin est. Phasellus pulvinar pretium quam ac volutpat. Duis ultricies tellus eget quam vulputate, facilisis dapibus libero ornare. Vestibulum hendrerit aliquam odio, eget cursus sem lacinia eu. Nunc magna metus, porta at massa quis, tempus rutrum ligula. Fusce arcu ipsum, aliquet vitae pulvinar id, egestas in mi.
+
+Proin egestas turpis eu elit ornare, sed dapibus libero lobortis. Vestibulum ultrices magna ac sollicitudin auctor. Sed finibus lacus eget metus ultrices, at efficitur elit pharetra. Suspendisse sit amet lorem consectetur, suscipit libero quis, accumsan nisi. Fusce ipsum metus, tempor at velit ac, laoreet efficitur mi. Vestibulum sed sollicitudin felis. Fusce eget maximus lectus. Nunc volutpat ac ante et tincidunt.
+
+Nullam non facilisis justo. Sed nec suscipit nisi, id gravida ipsum. Sed libero ex, tincidunt eget diam ac, cursus posuere arcu. Vestibulum tincidunt neque eu odio congue interdum. Quisque sed augue libero. Fusce vehicula neque non orci porttitor, eget vestibulum metus dictum. Curabitur sit amet nisl sem. Phasellus sed lorem dui. Phasellus quam lectus, varius id euismod vitae, maximus vitae massa. Sed vehicula massa non est feugiat facilisis. Mauris vel varius purus. Duis vitae neque et lacus ultrices porta. Nam ut sagittis lectus, eleifend porttitor libero. Nunc congue mi magna, eget pretium ipsum efficitur fringilla. Fusce odio eros, consectetur id vehicula viverra, rutrum at quam. </p>
       <Footer />
 
     </div>
