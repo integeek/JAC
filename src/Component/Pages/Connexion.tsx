bad61f2c7a15b091a48663320120665e58cece61
Ambre Giraud-Lanza <agiraudlanza@juniorisep.com>
2023-03-17 11:06:09 +0100

    ✅ page mot de passe oublié

--- src/Component/Pages/Connexion.tsx
+++ src/Component/Pages/Connexion.tsx
@@ -19,7 +19,7 @@ function Connexion() {
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">Votre email</label>
-                  <input type="email" name="email" id="email" className="bg-base-300 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required/>
+                  <input type="email" name="email" id="email" className="bg-base-300 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="nom@mail.com" required/>
                 </div>
                 <div>
                   <label htmlFor="password" className="block mb-2 text-sm font-medium text-black">Votre mot de passe</label>
@@ -34,9 +34,9 @@ function Connexion() {
                       <label htmlFor="remember" className="text-black">Se souvenir de moi</label>
                     </div>
                   </div>
-                  <a href="#" className="text-sm font-medium text-primary-600 hover:underline">Mot de passe oublié ?</a>
+                  <Link to="/mdpoublie" className="text-sm font-medium text-primary-600 hover:underline">Mot de passe oublié ?</Link>
                 </div>
-                <button type="submit" className="w-full text-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 bg-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Se connecter</button>
+                <button type="submit" className="w-full text-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 bg-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Se connecter</button>
                 <p className="text-sm font-light text-black">
                       Pas encore de compte ? <Link to="/inscription" className="font-medium text-primary-600 text-black hover:underline">S'inscrire</Link>
                 </p>
--- src/Component/Pages/Inscription.tsx
+++ src/Component/Pages/Inscription.tsx
@@ -19,7 +19,7 @@ function Inscription() {
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">Votre email</label>
-                  <input type="email" name="email" id="email" className="bg-base-300 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required/>
+                  <input type="email" name="email" id="email" className="bg-base-300 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="nom@mail.com" required/>
                 </div>
                 <div>
                   <label htmlFor="password" className="block mb-2 text-sm font-medium text-black">Mot de passe</label>
@@ -37,7 +37,7 @@ function Inscription() {
                     <label htmlFor="terms" className="font-medium text-black">J'accepte les <Link to="/mentions" className="font-medium text-black text-primary-600 hover:underline">termes et conditions d'utilisation générale</Link></label>
                   </div>
                 </div>
-                <button type="submit" className="w-full text-black bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">S'inscrire</button>
+                <button type="submit" className="w-full text-black bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">S'inscrire</button>
                 <p className="text-sm font-light text-black">
                       Déja un compte ? <Link to="/connexion" className="font-medium text-primary-600 hover:underline">Se connecter</Link>
                       
