d03d120d6a701b07b46f7c791ba99adc539c181f
Ambre Giraud-Lanza <agiraudlanza@juniorisep.com>
2023-05-06 12:08:49 +0200

    ✨ Ajout d'un captcha

--- src/Component/Pages/Authentification/Inscription.tsx
+++ src/Component/Pages/Authentification/Inscription.tsx
@@ -1,6 +1,31 @@
 import { Link } from "react-router-dom"
+import ReCAPTCHA from "react-google-recaptcha"
+import React, { useRef } from "react"
+import Axios from "../../../Axios"
+
+interface CaptchaRefObject {
+  getValue: () => string;
+  reset: () => void;
+}
 
 function Inscription() {
+  
+  const captchaRef = useRef<ReCAPTCHA>(null)
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const inputVal = (e.currentTarget.querySelector("input[name=\"inputVal\"]") as HTMLInputElement).value
+    const token = captchaRef.current?.getValue()
+    captchaRef.current?.reset()
+  
+    await Axios.post(inputVal, token)
+      .then((res) => console.log(res))
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  
+
   return (
     <div>
       <title>Inscription</title>
@@ -15,7 +40,16 @@ function Inscription() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl">
                   Inscription
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label htmlFor="prenom" className="block mb-2 text-sm font-medium text-black">Prénom</label>
+                  <input type="text" name="prenom" id="prenom" className="peer bg-base-300 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Votre prénom" required/>
+                </div>
+                <div>
+                  <label htmlFor="nom" className="block mb-2 text-sm font-medium text-black">Nom</label>
+                  <input type="text" name="nom" id="nom" className="peer bg-base-300 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Votre nom" required/>
+                </div>
+
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">Votre email</label>
                   <input type="email" name="email" id="email" className="peer bg-base-300 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="nom@mail.com" required/>
@@ -38,6 +72,11 @@ function Inscription() {
                     <label htmlFor="terms" className="font-medium text-black">J'accepte les <Link to="/mentions" className="font-medium underline text-black text-primary-600 hover:underline">termes et conditions d'utilisation générale</Link></label>
                   </div>
                 </div>
+                <input type="text" name="inputVal" />
+                <ReCAPTCHA
+                  sitekey={process.env.REACT_APP_SITE_KEY || ""}
+                  ref={captchaRef}
+                />
                 <button type="submit" className="w-full text-black bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">S'inscrire</button>
                 <p className="text-sm font-light text-black">
                       Déja un compte ? <Link to="/connexion" className="font-medium text-primary-600 hover:underline">Se connecter</Link>
